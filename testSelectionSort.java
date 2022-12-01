e60facbc57c9a53eb06ed4608c659990ab9eddeb
Fesler <jafesler@UMICH.EDU>
2022-12-01 18:22:22 -0500

    Develop positive and negative tests

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -1,16 +1,7 @@
 import static org.junit.Assert.*;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
-
-	
-	public void test() {
-		testPositive();
-		testNegative();
-		testMixed();
-		testDuplicates();
-	}
 	
     public testSelectionSort() {
     	
@@ -18,28 +9,62 @@ public class testSelectionSort {
     
 	@Test
     public void testPositive(){
-        int[] arr = new int[5];
+		int length = 5;
+		
+		/** Test data contains positive values only **/
+        int[] arr = new int[length];
         arr[0] = 8;
         arr[1] = 9;
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
         
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
+        int[] expectedSortedArr = new int[5];
+        expectedSortedArr[0] = 2;
+        expectedSortedArr[1] = 7;
+        expectedSortedArr[2] = 8;
+        expectedSortedArr[3] = 9;
+        expectedSortedArr[4] = 10;
         
+        SelectionSort selectionSort = new SelectionSort();
         
-        /** add tests to check for this unit test **/
+        int[] sortedArr = selectionSort.basicSelectionSort(arr);
+        
+        for(int i = 0; i < length; i++)
+        {
+        	assertSame("Test of only positives has failed at index: " + i, expectedSortedArr[i], sortedArr[i]);
+        }		
     }
     
 	@Test
     public void testNegative(){
         
-        /** Test data contains negative values only **/
+		int length = 5;
+		
+		/** Test data contains negative values only **/
+        int[] arr = new int[length];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+        
+        int[] expectedSortedArr = new int[length];
+        
+        expectedSortedArr[4] = -10;
+        expectedSortedArr[3] = -9;
+        expectedSortedArr[2] = -8;
+        expectedSortedArr[1] = -7;
+        expectedSortedArr[0] = -2;
+        
+        SelectionSort selectionSort = new SelectionSort();
+        
+        int[] sortedArr = selectionSort.basicSelectionSort(arr);
+        
+        for(int i = 0; i < length; i++)
+        {
+        	assertSame("Test of negatives only failed has failed at index: " + i, expectedSortedArr[i], sortedArr[i]);
+        }	
     }
     
 	@Test
